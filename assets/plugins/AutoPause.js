9a2f4a34589fe177e9cdc70a14087da4077aa701
Ramdhei <ramdhei2002@gmail.com>
2020-11-09 18:35:25 -0500

    Visibility Change yass

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,8 @@ class AutoPause {
     constructor() {
         this.threshold = 0.25
         this.handleIntersection = this.handleIntersection.bind(this)
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
+
     }
     run(player) {
         this.player = player
@@ -10,6 +12,8 @@ class AutoPause {
         })
 
         observer.observe(this.player.media)
+
+        document.addEventListener("visibilitychange", this.handleVisibilityChange)
     }
 
     handleIntersection(entries) {
@@ -17,6 +21,16 @@ class AutoPause {
         const isVisible = entry.intersectionRatio >= this.threshold
 
         if (isVisible) {
+            this.player.play()
+        } else {
+            this.player.pause()
+        }
+    }
+
+    handleVisibilityChange() {
+        const isVisible = document.visibilityState === 'visible'
+
+        if(isVisible) {
             this.player.play()
         } else {
             this.player.pause()
