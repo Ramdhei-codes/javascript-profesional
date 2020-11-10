fc7a6357afe23b872857b16d6b9dde64ce3ed554
Ramdhei <ramdhei2002@gmail.com>
2020-11-10 12:07:28 -0500

    Service Workers

--- assets/index.js
+++ assets/index.js
@@ -9,3 +9,9 @@ const toggleMute = document.querySelector('#mute')
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
 button.onclick = () => player.togglePlay()
 toggleMute.onclick = () => player.toggleMute()
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js').catch(error => {
+        console.log(error.message)
+    })
+}
