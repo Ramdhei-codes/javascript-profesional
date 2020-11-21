3687d56a82294181a48ec039c42e16f8e8950d7a
Ramdhei <ramdhei2002@gmail.com>
2020-11-20 22:15:14 -0500

    Finished course

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -1,6 +1,7 @@
-import MediaPlayer from './MediaPlayer'
-import AutoPlay from './plugins/AutoPlay'
-import AutoPause from './plugins/AutoPause'
+import MediaPlayer from '@ramdhei/mediaplayer'
+import AutoPlay from '@ramdhei/mediaplayer/lib/plugins/AutoPlay'
+import AutoPause from '@ramdhei/mediaplayer/lib/plugins/AutoPause'
+import Ads from '@ramdhei/mediaplayer/lib/plugins/Ads'
 
 const video = document.querySelector('video')
 const button: HTMLElement = document.querySelector('#play')
@@ -8,7 +9,7 @@ const toggleMute: HTMLElement = document.querySelector('#mute')
 
 const player = new MediaPlayer({
     el: video,
-    plugins: [new AutoPlay(), new AutoPause()]
+    plugins: [new AutoPlay(), new AutoPause(), new Ads()]
 });
 button.onclick = () => player.togglePlay()
 toggleMute.onclick = () => player.toggleMute()
